bdb2a3b9ab886f7de81b6c0ad726844d469ef127
zerofang <zerofangzerofang@outlook.com>
2018-04-09 23:14:25 +0800

    ch5

--- MLiA/ch5-logRegres/logRegres.py
+++ MLiA/ch5-logRegres/logRegres.py
@@ -3,7 +3,7 @@ from numpy import *
 def loadDataSet():
     dataMat = []
     labelMat = []
-    fr = open('textSet.txt')
+    fr = open('testSet.txt')
     for line in fr.readlines():
         lineArr = line.strip().split()
         dataMat.append([1.0,float(lineArr[0]),float(lineArr[1])])
@@ -25,3 +25,29 @@ def gradAscent(dataMatIn,classLabels):
         error = (labelMat - h)
         weights = weights + alpha * dataMatrix.transpose() * error
     return weights
+
+def plotBeatFit(weights):
+    import matplotlib.pyplot as plt
+    dataMat,labelMat = loadDataSet()
+    dataArr = array(dataMat)
+    n = shape(dataArr)[0]
+    xcord1 = []
+    ycord1 = []
+    xcord2 = []
+    ycord2 = []
+    for i in range(n):
+        if int(labelMat[i]) == 1:
+            xcord1.append(dataArr[i,1])
+            ycord1.append(dataArr[i,2])
+            xcord2.append(dataArr[i,1])
+            ycord2.append(dataArr[i,2])
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.scatter(xcord1,ycord1,s=30,c='red',marker='s')
+        ax.scatter(xcord2,ycord2,s=30,c='green')
+        x=arange(-3.0,3.0,0.1)
+        y = (-weights[0]-weights[1]*x)/weights[2]
+        ax.plot(x,y)
+        plt.xlabel('X1')
+        plt.ylabel('X2')
+        plt.show()
